2f9b35d7e3fbe55ccbba383ed6fac7caae85425a
Arnav Kumar <arnav63366@gmail.com>
2025-04-18 14:48:04 +0530

    Create defense_lawyer.py

---
+++ agents/defense_lawyer.py
@@ -0,0 +1,10 @@
+from langchain.chat_models import ChatGroq
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from config.prompts import DEFENSE_PROMPT
+from config.settings import GROQ_API_KEY, LLAMA_MODEL
+
+def get_defense_agent():
+    llm = ChatGroq(groq_api_key=GROQ_API_KEY, model=LLAMA_MODEL)
+    prompt = PromptTemplate.from_template(DEFENSE_PROMPT + "\nCase:\n{case_summary}")
+    return LLMChain(llm=llm, prompt=prompt)
