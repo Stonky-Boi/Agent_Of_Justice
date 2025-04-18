d37f216c288b9e8946c0d572144f6025af040422
Arnav Kumar <arnav63366@gmail.com>
2025-04-18 14:47:42 +0530

    Create prosecution_lawyer.py

---
+++ agents/prosecution_lawyer.py
@@ -0,0 +1,10 @@
+from langchain.chat_models import ChatGroq
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from config.prompts import PROSECUTION_PROMPT
+from config.settings import GROQ_API_KEY, LLAMA_MODEL
+
+def get_prosecution_agent():
+    llm = ChatGroq(groq_api_key=GROQ_API_KEY, model=LLAMA_MODEL)
+    prompt = PromptTemplate.from_template(PROSECUTION_PROMPT + "\nCase:\n{case_summary}")
+    return LLMChain(llm=llm, prompt=prompt)
