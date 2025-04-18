f50883835305a4b3ec98efce2777775a96cf36ac
Arnav Kumar <arnav63366@gmail.com>
2025-04-18 14:46:15 +0530

    Create judge.py

---
+++ agents/judge.py
@@ -0,0 +1,14 @@
+from langchain.chat_models import ChatGroq
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from config.prompts import JUDGE_PROMPT
+from config.settings import GROQ_API_KEY, LLAMA_MODEL
+
+def get_judge_agent():
+    llm = ChatGroq(groq_api_key=GROQ_API_KEY, model=LLAMA_MODEL)
+
+    prompt = PromptTemplate.from_template(
+        template=JUDGE_PROMPT + "\nCase:\n{case_summary}"
+    )
+
+    return LLMChain(llm=llm, prompt=prompt)
