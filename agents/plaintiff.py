f595aa1c302e684af8e9ec7dc3038af1dcd5c4ca
Arnav Kumar <arnav63366@gmail.com>
2025-04-18 17:01:09 +0530

    Update plaintiff.py

--- agents/plaintiff.py
+++ agents/plaintiff.py
@@ -1,10 +1,8 @@
 from langchain_groq import ChatGroq
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
 from config.prompts import PLAINTIFF_PROMPT
 from config.settings import GROQ_API_KEY, LLAMA_MODEL
+from agents.base_agent import CourtroomAgent
 
 def get_plaintiff_agent():
     llm = ChatGroq(groq_api_key=GROQ_API_KEY, model=LLAMA_MODEL)
-    prompt = PromptTemplate.from_template(PLAINTIFF_PROMPT + "\nCase:\n{case_summary}")
-    return LLMChain(llm=llm, prompt=prompt)
+    return CourtroomAgent(llm, PLAINTIFF_PROMPT, "Plaintiff")
