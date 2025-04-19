f0f8d9312764d4473dfaf66a46e9b11cb425c908
Arnav Kumar <arnav63366@gmail.com>
2025-04-19 16:35:38 +0530

    Create jury.py

---
+++ agents/jury.py
@@ -0,0 +1,14 @@
+# from langchain_groq import ChatGroq
+# from config.settings import GROQ_API_KEY, LLAMA_MODEL
+from langchain_google_genai import ChatGoogleGenerativeAI
+from config.settings import GOOGLE_API_KEY
+from config.prompts import JURY_PROMPT
+from agents.base_agent import CourtroomAgent
+
+def get_jury_agent():
+    # llm = ChatGroq(groq_api_key=GROQ_API_KEY, model=LLAMA_MODEL)
+    llm = ChatGoogleGenerativeAI(
+        google_api_key=GOOGLE_API_KEY,
+        model="gemini-2.0-flash-lite"  # or "models/gemini-pro" if needed
+    )
+    return CourtroomAgent(llm, JURY_PROMPT, "Jury")
